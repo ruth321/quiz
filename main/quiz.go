f67f78e4cccc261ed89885ed857f7f03f3b451c6
erik <erikkatsanov@gmail.com>
2020-07-17 19:09:02 +0300

    trash

--- main/quiz.go
+++ main/quiz.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type questionAnswer struct {
+	question string
+	answer   string
+}
+
+func main() {
+	k := true
+	for k {
+		fmt.Println("1. Start quiz")
+		fmt.Println("2. Add questions")
+		fmt.Println("3. Exit")
+		fmt.Print("->")
+		var a int
+		fmt.Scan(&a)
+		switch a {
+		case 1:
+
+		case 2:
+			file, err := ioutil.ReadFile("guizFile.json")
+			err = err
+			//			if errors.Is(err, "2") {
+
+			//			}
+			var quiz []questionAnswer
+			_ = json.Unmarshal(file, &quiz)
+			var rep string
+			fmt.Scan(&rep)
+			if rep == "y" {
+
+			}
+
+		case 3:
+			k = false
+
+		}
+
+	}
+
+}
